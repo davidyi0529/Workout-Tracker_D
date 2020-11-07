dd096818c0b62c47574242fd5052bbcfd868d585
David Yi <david.yi.0529@gmail.com>
2020-11-07 00:53:17 -0800

    deployment

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser:true,
-    useFindAndModify:false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 require("./routes/api-routes")(app);
